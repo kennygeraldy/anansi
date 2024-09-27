6e0a51a78cd16c652d83c93befa0033624497bff
KGeraldy <kennygeraldy@gmail.com>
2024-09-27 15:40:39 +0700

    Add story4

--- exercise/day-1/HelloWorld/src/Main.kt
+++ exercise/day-1/HelloWorld/src/Main.kt
@@ -5,7 +5,8 @@ fun main() {
         units <= 100 -> units * 1.0
         units in 101..200 -> (100 * 1.0) + ((units - 100) * 1.5)
         units > 200 -> (100 * 1.0) + (100 * 1.5) + ((units - 200) * 2.0)
-        else -> 0.0
+        else ->
+            println("Insert the correct units")
     }
 
     println("Total electricity bill: $$charge")
