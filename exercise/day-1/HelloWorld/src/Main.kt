5d93facc0f60b30293405ba8959c56668caeb119
KGeraldy <kennygeraldy@gmail.com>
2024-09-27 15:37:48 +0700

    Add story4

--- exercise/day-1/HelloWorld/src/Main.kt
+++ exercise/day-1/HelloWorld/src/Main.kt
@@ -1,19 +1,12 @@
 fun main() {
-    val spent = 200;
+    val units = 250
 
-    val discount = when {
-        spent > 200 -> spent * 0.30
-        spent > 100 && spent <= 200 -> spent * 0.20
-        spent == 200 -> spent * 0.20
-        spent == 50 && spent <= 100 -> spent * 0.10
-        spent == 100 -> spent * 0.10
-        spent < 50 -> spent.toDouble()
-        else -> {
-            println("No Bonuses")
-            0.0
-        }
+    val charge = when {
+        units <= 100 -> units * 1.0
+        units in 101..200 -> (100 * 1.0) + ((units - 100) * 1.5)
+        units > 200 -> (100 * 1.0) + (100 * 1.5) + ((units - 200) * 2.0)
+        else -> 0.0
     }
 
-    val finalPrice = spent - discount;
-    println("Final price: $finalPrice")
+    println("Total electricity bill: $$charge")
 }
