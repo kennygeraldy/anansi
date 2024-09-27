20bab68546e74db341e369acdb16aec205423a4f
KGeraldy <kennygeraldy@gmail.com>
2024-09-27 14:43:44 +0700

    add story-1

--- exercise/day-1/HelloWorld/src/Main.kt
+++ exercise/day-1/HelloWorld/src/Main.kt
@@ -1,3 +1,18 @@
 fun main() {
-    println("Hello World!")
+    var score = arrayOf(50, 65,75,85,95);
+    for (x in score) {
+        if (x >= 90) {
+            println("Score = A")
+        } else if (x >= 80 && x < 90) {
+            println("Score = B")
+        }else if (x >= 70 && x < 80) {
+            println("Score = C")
+        }else if (x >= 60 && x < 69) {
+            println("Score = D")
+        }else if (x < 60) {
+            println("Score = F")
+        } else {
+            println("Insert valid score;")
+        }
+    }
 }
